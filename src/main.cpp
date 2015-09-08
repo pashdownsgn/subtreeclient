1e81aa2c14d272c790bc3126fadd37b4e3bed3e5
pashdownsgn <paul@sgn.com>
2015-09-08 14:33:59 -0700

    First real commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  subtreetest
+//
+//  Created by Paul Ashdown on 9/8/15.
+//  Copyright (c) 2015 Paul. All rights reserved.
+//
+
+#include <iostream>
+
+#include <test/test.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	test();
+    return 0;
+}
