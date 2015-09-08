5777db4fad3865204851bbbfe7a0f7148414cd7b
pashdownsgn <paul@sgn.com>
2015-09-08 14:35:55 -0700

    Squashed 'thirdparty/libtest/' content from commit fe70154
    git-subtree-dir: thirdparty/libtest
    git-subtree-split: fe701544363258b0729ed655dc9ed1c380add37b

---
+++ include/test/test.h
@@ -0,0 +1,14 @@
+//
+//  test.h
+//  libtest
+//
+//  Created by Paul Ashdown on 9/8/15.
+//  Copyright (c) 2015 Paul. All rights reserved.
+//
+
+#ifndef libtest_test_h
+#define libtest_test_h
+
+extern void test ();
+
+#endif
